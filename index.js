38b15597635ae7981e3b48b1b336e0e6fe7eb516
Rolo <46386318+Its-Rolo@users.noreply.github.com>
2024-07-19 03:09:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+distanceESPvideo = document.getElementById("distanceESPvideo");
+recoilVideo = document.getElementById("recoilVideo");
+zoomVideo = document.getElementById("zoomVideo");
+
 document.addEventListener("DOMContentLoaded", function() {
+  const fadeElement = document.getElementById("Title");
+  
+  // Add the 'fade-in' class to trigger the CSS animation
+  fadeElement.classList.add("fade-in");
+
   const textContainer = document.getElementById("SubTitle");
-  const texts = ["Best ShellShock.io Cheat", "Best ShellShock.io Hack", "Best ShellShock.io Mod"];
+  const texts = ["Cheapest Rust Script", "Cheapest Rust Utility", "Cheapest Rust Software"];
   let currentIndex = 0;
 
   function changeText() {
@@ -12,6 +21,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 1500); // Wait for 1.5 seconds (equal to the transition duration)
     setTimeout(changeText, 3000); // Repeat after 3 seconds
   }
+
+  distanceESPvideo.volume = 0;
+  distanceESPvideo.play();
+  distanceESPvideo.loop = true;
+
+  recoilVideo.volume = 0;
+  recoilVideo.play();
+  recoilVideo.loop = true;
+
+  zoomVideo.volume = 0;
+  zoomVideo.play();
+  zoomVideo.loop = true;
 
   // Initial call
   changeText(); // Start the text change function
